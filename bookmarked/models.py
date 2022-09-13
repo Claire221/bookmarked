3ff8a9e826937364bdc54aec504f329a00b205a6
Claire221 <c.biglands@hotmail.com>
2022-09-13 12:18:11 +0000

    Add code to add created_by to bookshelves

--- bookmarked/models.py
+++ bookmarked/models.py
@@ -5,9 +5,10 @@ class Bookshelves(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bookshelf_name = db.Column(db.String(25), unique=True, nullable=False)
     bookshelf_description = db.Column(db.Text, nullable=False)
+    created_by = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
-        return self.bookshelf_name 
+        return self.created_by
 
 
 class Users(db.Model):
@@ -15,5 +16,6 @@ class Users(db.Model):
     username = db.Column(db.String(8), unique=True, nullable=False)
     password = db.Column(db.String(8), unique=True, nullable=False)
 
+    
     def __repr__(self):
         return self.username
