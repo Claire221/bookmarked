6034f842f4e7ed743da29fbe6f4ce8e9c21afd25
Claire221 <c.biglands@hotmail.com>
2022-09-13 10:11:32 +0000

    Update local and deployed configuration

--- bookmarked/__init__.py
+++ bookmarked/__init__.py
@@ -7,16 +7,19 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-uri = os.environ.get("DB_URL")
-if uri.startswith("postgres:///"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
