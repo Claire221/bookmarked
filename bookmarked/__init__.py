db5a4f6b9b76b7182de6a3bf2ca22afb9a4bccc4
Claire221 <c.biglands@hotmail.com>
2022-09-13 09:03:43 +0000

    Fixed typo

--- bookmarked/__init__.py
+++ bookmarked/__init__.py
@@ -14,7 +14,7 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 uri = os.environ.get("DB_URL")
-if uri.startswith("postgres://"):
+if uri.startswith("postgres:///"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
