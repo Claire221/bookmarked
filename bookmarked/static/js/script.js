3e8461768d48ebf08c99ba1a0346dfd68149c832
Claire221 <c.biglands@hotmail.com>
2022-12-02 10:01:10 +0000

    Add models to delete books/bookshelves

--- bookmarked/static/js/script.js
+++ bookmarked/static/js/script.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+
+    console.log(elems)
 });
